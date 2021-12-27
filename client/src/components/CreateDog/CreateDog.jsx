69139b9aa5a21ceba8aae86c6ba702316cbd7567
Agustin Sartorio <sartorio85@gmail.com>
2021-12-27 20:48:57 -0300

    Work in process

--- client/src/components/CreateDog/CreateDog.jsx
+++ client/src/components/CreateDog/CreateDog.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTemperaments, postDog } from '../../actions/index';
+import styles from './CreateDog.module.css';
+import perro from '../../fondos/create.jpg'
 
 export default function CreateDog() {
     const dispatch = useDispatch();
@@ -18,7 +20,7 @@ export default function CreateDog() {
     console.log(weight);
 
     const handleHeight = (e) => {
-        if(e.target.name === 'min-height') {
+        if (e.target.name === 'min-height') {
             setHeight((prev) => [e.target.value, prev[1]])
         } else {
             setHeight((prev) => [prev[0], e.target.value])
@@ -26,7 +28,7 @@ export default function CreateDog() {
     }
 
     const handleWeight = (e) => {
-        if(e.target.name === 'min-weight') {
+        if (e.target.name === 'min-weight') {
             setWeight((prev) => [e.target.value, prev[1]])
         } else {
             setWeight((prev) => [prev[0], e.target.value])
@@ -44,7 +46,7 @@ export default function CreateDog() {
         e.preventDefault();
         const finalObj = {
             ...input,
-            weight:weight.join(' - '),
+            weight: weight.join(' - '),
             height: height.join(' - ')
         }
         console.log(finalObj);
@@ -61,9 +63,9 @@ export default function CreateDog() {
 
     function handleSelect(e) {
         /* console.log(e.target.value); */
-        const foundTemperament = temperaments.find((t) => t.id === Number(e.target.value)); 
+        const foundTemperament = temperaments.find((t) => t.id === Number(e.target.value));
         console.log(foundTemperament);
-        const temperamentObj = {name: foundTemperament.name, id: e.target.value};
+        const temperamentObj = { name: foundTemperament.name, id: e.target.value };
         setInput({
             ...input,
             temperament: [...input.temperament, temperamentObj]
@@ -76,80 +78,94 @@ export default function CreateDog() {
 
     return (
         <div>
-            <Link to="/home"><button >Perripedia</button></Link>
-            <div>
-                <div>
-                    <h1>Creá tu raza!</h1>
+            <div className={styles.divFijo}>
+
+                <h1 className={styles.dogi}>DOGIPEDIA🐾</h1>
+
+                <div className={styles.divbotones}>
+                    <Link to="/home"><button className={styles.recargar}>Home</button></Link>
+                    <Link to="./dog" /* className={styles.recargar} */><button className={styles.recargar}>Create Breed</button></Link>
                 </div>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <div>
-                        <label>Nombre:</label>
-                        <input
-                            type="text"
-                            value={input.name}
-                            name="name"
-                            onChange={(e) => handleChange(e)}
-                        />
+            </div>
+            <div className={styles.todo}>
+                <div className={styles.formulario}>
+                    <div className={styles.image}>
+                        <img src={perro} alt='img' />
                     </div>
-                    <div>
-                        <label>Esperanza de vida:</label>
-                        <input
-                            type="text"
-                            value={input.life_span}
-                            name="life_span"
-                            onChange={(e) => handleChange(e)}
-                        />
+                    <div className={styles.formulario2}>
+                        <h1>Create your own breed!</h1>
+                        <form onSubmit={(e) => handleSubmit(e)}>
+                            <div>
+                                <label>Name:</label>
+                                <input
+                                    type="text"
+                                    value={input.name}
+                                    name="name"
+                                    onChange={(e) => handleChange(e)}
+                                />
+                            </div>
+                            <div>
+                                <label>Life span:</label>
+                                <input
+                                    type="text"
+                                    value={input.life_span}
+                                    name="life_span"
+                                    onChange={(e) => handleChange(e)}
+                                />
+                            </div>
+                            <div>
+                                <label>Min weight (kg):</label>
+                                <input
+                                    type="number"
+                                    value={weight[0]}
+                                    name="min-weight"
+                                    onChange={handleWeight}
+                                />
+                            </div>
+                            <div>
+                                <label>Max weight (kg):</label>
+                                <input
+                                    type="number"
+                                    value={weight[1]}
+                                    name="max-weight"
+                                    onChange={handleWeight}
+                                />
+                            </div>
+                            <div>
+                                <label>Min height (cm):</label>
+                                <input
+                                    type="number"
+                                    value={height[0]}
+                                    name="min-height"
+                                    onChange={handleHeight}
+                                />
+                            </div>
+                            <div>
+                                <label>Max height (cm):</label>
+                                <input
+                                    type="number"
+                                    value={height[1]}
+                                    name="max-height"
+                                    onChange={handleHeight}
+                                />
+                            </div>
+                            <div>
+                                <label>Temperaments: </label>
+                                <select onChange={(e) => handleSelect(e)}>
+                                    {temperaments.map((t) => {
+                                        return <option value={t.id}>{t.name}</option>
+                                    })}
+                                </select>
+                                <ul><p>{input.temperament.map(el => el.name + ", ")}</p></ul>
+                            </div>
+                            <div className={styles.btn}>
+                                <button className={styles.crear} type="submit">
+                                    Create Breed
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                        <label>Peso minimo:</label>
-                        <input
-                            type="number"
-                            value={weight[0]}
-                            name="min-weight"
-                            onChange={handleWeight}
-                        />
-                    </div>
-                    <div>
-                        <label>Peso maximo:</label>
-                        <input
-                            type="number"
-                            value={weight[1]}
-                            name="max-weight"
-                            onChange={handleWeight}
-                        />
-                    </div>
-                    <div>
-                        <label>Altura minima:</label>
-                        <input
-                            type="number"
-                            value={height[0]}
-                            name="min-height"
-                            onChange={handleHeight}
-                        />
-                    </div>
-                    <div>
-                        <label>Altura maxima:</label>
-                        <input
-                            type="number"
-                            value={height[1]}
-                            name="max-height"
-                            onChange={handleHeight}
-                        />
-                    </div>
-                    <div>
-                        <select onChange={(e) => handleSelect(e)}>
-                            {temperaments.map((t) => {
-                                return <option value={t.id}>{t.name}</option>
-                            })}
-                        </select>
-                        <ul><p>{input.temperament.map(el => el.name + ", ")}</p></ul>
-                    </div>
-                    <div>
-                        <button type="submit">
-                            Crear raza
-                        </button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     )
